bb521b5e5e2c50bcef46be97d3e86d1d3a67ad21
Magdalene Matey <magdalenematey8@gmail.com>
2025-11-15 05:22:54 -0800

    Add library management system implementation

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,48 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False   # private availability flag
+
+    def check_out(self):
+        """Mark book as checked out."""
+        self._is_checked_out = True
+
+    def return_book(self):
+        """Mark book as available."""
+        self._is_checked_out = False
+
+    def is_available(self):
+        """Check if the book is available."""
+        return not self._is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self._books = []  # private list of Book objects
+
+    def add_book(self, book):
+        """Add a Book instance to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by title, if available."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return True
+        return False
+
+    def return_book(self, title):
+        """Return a book by title."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return True
+        return False
+
+    def list_available_books(self):
+        """Print all available books."""
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
